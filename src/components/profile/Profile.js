f4ab179c3e30a2d11a9613cd86126aff6d94f619
Steven Yao <51345129+skyao2002@users.noreply.github.com>
2020-09-19 01:02:32 -0500

    fix profile page error

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,54 +6,10 @@ import firebase from "firebase";
 
 const Profile = () => {
   return (
-    <div id='profile'>
-        <div id = "profile_col">
-            <div className = "profile_sep">
-                <h1>Profile</h1>
-            </div>
-            <div className = "profile_sep">
-                <img
-                src={firebase.auth().currentUser.photoURL}
-                id="profile_img"
-                alt="Profile"
-                >
-                </img>
-            </div>
-            <br/>
-            <div className="profile_sep">
-                <label>Name</label>
-                <input
-                    placeholder={
-                    firebase.auth().currentUser.displayName
-                        ? firebase.auth().currentUser.displayName
-                        : "Enter Name"
-                    }
-                ></input>
-            </div>
-            <br/>
-            <div className = "profile_sep">
-                <input placeholder = "phone number"></input>
-            </div>
-            <br/>
-            <div className = "profile_sep">
-                <label>Date of Birth</label>
-                <input type="date"></input>
-            </div>
-            
-            <br/>
-            <div className = "profile_sep">
-                <label>Pharmacy</label>
-                <input placeholder = "Address"></input>
-                <input placeholder = "City"></input>
-                <input placeholder = "State"></input>
-                <input type = "number" placeholder = "PIN"></input>
-            </div>
-            
-            <br/>
-            <div className = "profile_sep">
-                <label>Insurance</label>
-                <input placeholder = "ex. Blue Cross Blue Shield"></input>
-            </div>
+    <div id="profile">
+      <div id="profile_col">
+        <div className="profile_sep">
+          <h1>Profile</h1>
         </div>
         <div className="profile_sep">
           <img
@@ -101,14 +57,18 @@ const Profile = () => {
         <br />
         <div className="profile_sep">
           <label>Pharmacy</label>
-          <input placeholder="999-999-999"></input>
+          <input placeholder="Address"></input>
+          <input placeholder="City"></input>
+          <input placeholder="State"></input>
+          <input type="number" placeholder="PIN"></input>
         </div>
 
         <br />
         <div className="profile_sep">
           <label>Insurance</label>
-          <input placeholder="999-999-999"></input>
+          <input placeholder="ex. Blue Cross Blue Shield"></input>
         </div>
+      </div>
     </div>
   );
 };
