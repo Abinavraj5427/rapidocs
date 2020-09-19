28923ac9913c1e63f8ff8813ef9870759e831a8c
Aman Singh <aman@little-mac.local>
2020-09-19 11:54:39 -0500

    new navbar

--- src/components/routing/Routes.js
+++ src/components/routing/Routes.js
@@ -1,16 +1,18 @@
-import React, { Profiler } from "react";
-import { Route, Switch } from "react-router-dom";
-import Records from "../records/Records";
-import Profile from "../profile/Profile";
-import VerifyPhone from "../profile/VerifyPhone";
+import React, { Profiler } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Records from '../records/Records';
+import Profile from '../profile/Profile';
+import VerifyPhone from '../profile/VerifyPhone';
+import MyDoctor from '../forms/MyDoctor';
 
 const Routes = () => {
   return (
-    <section className="container">
+    <section className='container'>
       <Switch>
-        <Route exact path="/records" component={Records} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/verifyphone" component={VerifyPhone} />
+        <Route exact path='/records' component={Records} />
+        <Route exact path='/doctors' component={MyDoctor} />
+        <Route exact path='/profile' component={Profile} />
+        <Route exact path='/verifyphone' component={VerifyPhone} />
       </Switch>
     </section>
   );
