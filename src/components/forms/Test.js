fcdb1f653c79e1106069564520ed911d3e99b781
kmkhami <nicholas.khami@gmail.com>
2020-09-19 11:54:56 -0500

    File upload is workign asynchronously in test.js

--- src/components/forms/Test.js
+++ src/components/forms/Test.js
@@ -25,36 +25,38 @@ const TestForm = props => {
               const inputField = document.getElementById('fileInputID'); 
               let selectedFile = inputField.files[0];
               let storageRef = firebase.storage().ref(id  + "/tests/" + selectedFile.name); 
-              var upload = storageRef.put(selectedFile);
+              const uploadTask = storageRef.put(selectedFile);
               //update progress bar
-              upload.on(
-                  "state_changed",
-                  function progress(snapshot) {
-                      var percentage =
-                          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                      //spinner can go here
-                  },
-
-                  function error() {
-                      alert("error uploading file");
-                  },
-
-                  function complete() {
-                      //remove spinner
-                      alert("File is uploaded"); 
+              uploadTask.on('state_changed', function (snapshot) {
+                  // Observe state change events such as progress, pause, and resume
+                  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                  var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                  console.log('Upload is ' + progress + '% done');
+                  switch (snapshot.state) {
+                      case firebase.storage.TaskState.PAUSED: // or 'paused'
+                          console.log('Upload is paused');
+                          break;
+                      case firebase.storage.TaskState.RUNNING: // or 'running'
+                          console.log('Upload is running');
+                          break;
                   }
-              );
-              storageRef.getDownloadURL().then(function (url) {
-                  const key = firebase
-                      .database()
-                      .ref()
-                      .child('patients/' + id + '/tests')
-                      .push().key;
-                  let updates = {};
-                  updates['patients/' + id + '/tests/' + key] = { title: formData.title, date: formData.date, fileName: url };
-                  firebase.database().ref().update(updates);
-                  window.location = 'http://localhost:3000/profile'; 
-              }); 
+              }, function (error) {
+                  // Handle unsuccessful uploads
+              }, function () {
+                  // Handle successful uploads on complete
+                  uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+                      const key = firebase
+                          .database()
+                          .ref()
+                          .child('patients/' + id + '/tests')
+                          .push().key;
+                      let updates = {};
+                      updates['patients/' + id + '/tests/' + key] = { title: formData.title, date: formData.date, fileName: downloadURL
+                          };
+                      firebase.database().ref().update(updates);
+                      window.location = 'http://localhost:3000/profile'; 
+                  });
+              });
         }
       }}
     >
