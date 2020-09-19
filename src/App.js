d7487d978e4db0a921c907ed6b0b6142b9dac4c0
Aman Singh <aman@little-mac.local>
2020-09-19 16:01:44 -0500

    made landing page accessible

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Fragment, useEffect, useState, useContext } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./styles/App.css";
-import Routes from "./components/routing/Routes";
-import Landing from "./components/layout/Landing";
-import Navbar from "./components/layout/Navbar";
-import firebase from "firebase";
-import MenuProvider from "react-flexible-sliding-menu";
-import Menu from "./components/layout/Menu";
-
+import React, { Fragment, useEffect, useState, useContext } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './styles/App.css';
+import Routes from './components/routing/Routes';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import firebase from 'firebase';
+import MenuProvider from 'react-flexible-sliding-menu';
+import Menu from './components/layout/Menu';
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(user => {
       //true if user is an object, false otherwise
       setIsSignedIn(!!user);
     });
@@ -27,10 +26,10 @@ const App = () => {
   return (
     <Router>
       <Fragment>
-        <MenuProvider MenuComponent={Menu} animation="push">
+        <MenuProvider MenuComponent={Menu} animation='push'>
           <Navbar />
           <Switch>
-            <Route exact path="/" componenEt={Landing} />
+            <Route exact path='/' component={Landing} />
             <Route component={Routes} />
           </Switch>
         </MenuProvider>
--- src/components/routing/Routes.js
+++ src/components/routing/Routes.js
@@ -1,24 +1,21 @@
-import React, { Profiler } from "react";
-import { Route, Switch } from "react-router-dom";
-import Records from "../records/Records";
-import Profile from "../profile/Profile";
-import VerifyPhone from "../profile/VerifyPhone";
-import MyDoctor from "../forms/MyDoctor";
-import Procedure from "../forms/Procedure";
-import Medication from "../forms/Medication";
+import React, { Profiler } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Records from '../records/Records';
+import Profile from '../profile/Profile';
+import VerifyPhone from '../profile/VerifyPhone';
+import MyDoctor from '../forms/MyDoctor';
+import Procedure from '../forms/Procedure';
+import Medication from '../forms/Medication';
 
 const Routes = () => {
   return (
-    <section className = "container">
+    <section className='container'>
       <Switch>
-        <Route exact path="/records" component={Records} />
-        <Route exact path="/doctors" component={MyDoctor} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/verifyphone" component={VerifyPhone} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/docs" component={MyDoctor} />
-        <Route path="/procedures" component={Procedure} />
-        <Route path="/meds" component={Medication} />
+        <Route exact path='/records' component={Records} />
+        <Route exact path='/doctors' component={MyDoctor} />
+        <Route exact path='/profile' component={Profile} />
+        <Route exact path='/verifyphone' component={VerifyPhone} />
+        <Route path='/profile' component={Profile} />
       </Switch>
     </section>
   );
