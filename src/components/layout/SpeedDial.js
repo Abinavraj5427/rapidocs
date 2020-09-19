7b98380be056733a2838e14a2a9b4b73696f7534
Aman Singh <aman@little-mac.local>
2020-09-19 18:55:06 -0500

    FIXED TOOLTIP

--- src/components/layout/SpeedDial.js
+++ src/components/layout/SpeedDial.js
@@ -6,72 +6,119 @@ import { faAllergies } from '@fortawesome/free-solid-svg-icons';
 import { faCommentMedical } from '@fortawesome/free-solid-svg-icons';
 import { faProcedures } from '@fortawesome/free-solid-svg-icons';
 import { faCalendar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Backdrop from '@material-ui/core/Backdrop';
 import SpeedDial from '@material-ui/lab/SpeedDial';
 import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
 import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
 
-const useStyles = makeStyles((theme) => ({
-    speedDial: {
-        position: 'absolute',
-        bottom: '4%',
-        right: '3%',
-        zIndex: theme.zIndex.drawer + 2,
-    },
-    backDrop: {
-        color: 'rgba(0, 0, 0, 1.0)',
-    },
+const useStyles = makeStyles(theme => ({
+  speedDial: {
+    position: 'absolute',
+    bottom: '4%',
+    right: '3%',
+    zIndex: theme.zIndex.drawer + 2,
+  },
+  backDrop: {
+    color: 'rgba(0, 0, 0, 1.0)',
+  },
 }));
 
-const actions = [
-    { icon: <FontAwesomeIcon icon={faVial} />, name: 'Test' },
-    { icon: <FontAwesomeIcon icon={ faPills } />, name: 'Medicication' },
-    { icon: <FontAwesomeIcon icon={faAllergies} />, name: 'Allergies' },
-    { icon: <FontAwesomeIcon icon={faCommentMedical} />, name: 'Diagnosis' },
-    { icon: <FontAwesomeIcon icon={faProcedures} />, name: 'Procedure' },
-    { icon: <FontAwesomeIcon icon={faCalendar} />, name: 'Appointment' }
-];
+const SpeedDialToolTip = ({ setCurrentForm }) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [hidden, setHidden] = React.useState(false);
 
-export default function SpeedDialTooltipOpen() {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-    const [hidden, setHidden] = React.useState(false);
+  const actions = [
+    {
+      icon: (
+        <FontAwesomeIcon icon={faVial} onClick={() => setCurrentForm('Test')} />
+      ),
+      name: 'Test',
+    },
+    {
+      icon: (
+        <FontAwesomeIcon
+          icon={faPills}
+          onClick={() => setCurrentForm('Medication')}
+        />
+      ),
+      name: 'Medication',
+    },
+    {
+      icon: (
+        <FontAwesomeIcon
+          icon={faAllergies}
+          onClick={() => setCurrentForm('Allergy')}
+        />
+      ),
+      name: 'Allergy',
+    },
+    {
+      icon: (
+        <FontAwesomeIcon
+          icon={faCommentMedical}
+          onClick={() => setCurrentForm('Diagnosis')}
+        />
+      ),
+      name: 'Diagnosis',
+    },
+    {
+      icon: (
+        <FontAwesomeIcon
+          icon={faProcedures}
+          onClick={() => setCurrentForm('Procedure')}
+        />
+      ),
+      name: 'Procedure',
+    },
+    {
+      icon: (
+        <FontAwesomeIcon
+          icon={faCalendar}
+          onClick={() => setCurrentForm('Appointment')}
+        />
+      ),
+      name: 'Appointment',
+    },
+  ];
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const style = {
-        zIndex: 100,
-        }
+  const style = {
+    zIndex: 100,
+  };
 
-    return (
-        <div className={classes.root}>
-            <Backdrop style={style} open={open} />
-            <SpeedDial
-                ariaLabel="SpeedDial tooltip example"
-                className={classes.speedDial}
-                hidden={hidden}
-                icon={<SpeedDialIcon />}
-                onClose={handleClose}
-                onOpen={handleOpen}
-                open={open}
-            >
-                {actions.map((action) => (
-                    <SpeedDialAction
-                        key={action.name}
-                        icon={action.icon}
-                        tooltipTitle={action.name}
-                        tooltipOpen
-                        onClick={handleClose}
-                    />
-                ))}
-            </SpeedDial>
-        </div>
-    );
-}
+  return (
+    <div className={classes.root}>
+      <Backdrop style={style} open={open} />
+      <SpeedDial
+        ariaLabel='SpeedDial tooltip example'
+        className={classes.speedDial}
+        hidden={hidden}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map(action => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            tooltipOpen
+            onClick={handleClose}
+          />
+        ))}
+      </SpeedDial>
+    </div>
+  );
+};
+
+export default SpeedDialToolTip;
--- src/components/records/Records.js
+++ src/components/records/Records.js
@@ -5,30 +5,23 @@ import Medication from '../forms/Medication';
 import Allergy from '../forms/Allergy';
 import Diagnosis from '../forms/Diagnosis';
 import Appointment from '../forms/Appointment';
-import SpeedDial from '../layout/SpeedDial'; 
-import ReactDOM from "react-dom";
-import { Link, useLocation, BrowserRouter as Router } from "react-router-dom";
-
-function useQuery() {
-    return new URLSearchParams(useLocation().search); 
-}
+import SpeedDialToolTip from '../layout/SpeedDial';
+import ReactDOM from 'react-dom';
+import { Link, useLocation, BrowserRouter as Router } from 'react-router-dom';
 
 const Records = () => {
-    let query = useQuery(); 
-    const currentForm = query.get("form"); 
-    //const [currentForm, setCurrentForm] = useState('Test');
-  
+  const [currentForm, setCurrentForm] = useState('Test');
 
   return (
     <div>
-      <SpeedDial />
-      <p className='M'>Medical Records</p>
-      {currentForm == 'Test' && <Test />}
-      {currentForm == 'Procedure' && <Procedure />}
-      {currentForm == 'Medication' && <Medication />}
-      {currentForm == 'Allergy' && <Allergy />}
-      {currentForm == 'Diagnosis' && <Diagnosis />}
-      {currentForm == 'Appointment' && <Appointment />}
+      <SpeedDialToolTip setCurrentForm={setCurrentForm} />
+      {/* <p className='M'>Medical Records</p> */}
+      {currentForm === 'Test' && <Test />}
+      {currentForm === 'Procedure' && <Procedure />}
+      {currentForm === 'Medication' && <Medication />}
+      {currentForm === 'Allergy' && <Allergy />}
+      {currentForm === 'Diagnosis' && <Diagnosis />}
+      {currentForm === 'Appointment' && <Appointment />}
     </div>
   );
 };
