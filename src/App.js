d27ad65d03a2d8bd130fe27e015fd6828020ce76
Steven Yao <51345129+skyao2002@users.noreply.github.com>
2020-09-18 22:40:11 -0500

    added test in app.js, added react firebase modules in package

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './styles/App.css';
-import Routes from './components/routing/Routes';
-import Landing from './components/layout/Landing';
-import Navbar from './components/layout/Navbar';
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles/App.css";
+import Routes from "./components/routing/Routes";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+import db from "./firebase";
 
 const App = () => {
+  useEffect(() => {
+    db.ref("patients/7139923350")
+      .once("value")
+      .then(function (snapshot) {
+        console.log(snapshot.val());
+      });
+  }, []);
   return (
     <Router>
       <Fragment>
         <Navbar />
         <Switch>
-          <Route exact path='/' component={Landing} />
+          <Route exact path="/" component={Landing} />
           <Route component={Routes} />
         </Switch>
       </Fragment>
