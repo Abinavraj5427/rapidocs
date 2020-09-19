cad29909d01df9148173c5726c5f65ac4b333f51
Aman Singh <aman@little-mac.local>
2020-09-18 23:05:22 -0500

    resolve merge conflict

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,18 +1,13 @@
 import React from 'react';
-// import { Image, Col, FormControl } from 'react-bootstrap';
-import InputGroup from 'react-bootstrap/InputGroup'
 import profile from '../../assets/Design1.PNG';
 
-
-const Landing = () => {
+const Profile = () => {
   return (
-    <div id = "profile">
-        <h1>Profile</h1>
-        <img src = {profile} id = "profile_img" alt = "Profile Image"></img>
-
-            
+    <div id='profile'>
+      <h1>Profile</h1>
+      <img src={profile} id='profile_img' alt='Profile Image'></img>
     </div>
   );
 };
 
-export default Landing;
+export default Profile;
