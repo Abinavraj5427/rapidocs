d060f103efcfc93f0f531aadb0b32ecb683a50ac
Steven Yao <51345129+skyao2002@users.noreply.github.com>
2020-09-18 23:56:59 -0500

    authentication and is signed in check on navbar

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -4,33 +4,17 @@ import firebaseui from "firebaseui";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 const Landing = () => {
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
   //configuration for the google, email, and phone authentication
   const uiConfig = {
     signInFlow: "popup",
     signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
       signInSuccess: () => false,
     },
   };
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      //true if user is an object, false otherwise
-      setIsSignedIn(!!user);
-    });
-  }, []);
-
-  // tests whether isSignedIn changed
-  // useEffect(() => {
-  //   console.log(isSignedIn);
-  //   if (isSignedIn) console.log(firebase.auth().currentUser.displayName);
-  // }, [isSignedIn]);
 
   return (
     <div className="landing">
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import firebase from "firebase";
 
 const Navbar = () => {
+  console.log("hi\n\n\n\n" + firebase.auth().currentUser);
   return (
-    <nav className='navbar'>
-      <Link to='/'>Logo</Link>
-      <div className='links'>
-        <Link to='/records' className='link S'>
-          Records
-        </Link>
-        <Link to='/profile' className='link S'>
-          Profile
-        </Link>
-      </div>
+    <nav className="navbar">
+      <Link to="/">Logo</Link>
+      {firebase.auth().currentUser != null && (
+        <div className="links">
+          <Link to="/records" className="link S">
+            Records
+          </Link>
+          <Link to="/profile" className="link S">
+            Profile
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
