6138a538289508bcdcf13aafe6a0607e7ba816b2
Abinavraj5427 <abinavraj10@gmail.com>
2020-09-20 02:31:34 -0500

    Added server.js hosting with Express and NodeJS

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http')
+const server = http.createServer(app)
+const path = require('path');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+});
+
