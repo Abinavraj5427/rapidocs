6e6d911c36e8ec2aba560dfc8c8ce668eb23f3f0
kmkhami <nicholas.khami@gmail.com>
2020-09-19 23:27:55 -0500

    Timeline Debugging Commit

--- src/components/layout/Timeline.js
+++ src/components/layout/Timeline.js
@@ -11,12 +11,10 @@ import { faProcedures } from '@fortawesome/free-solid-svg-icons';
 import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-let TimelineRoot = document.createElement('VerticalTimeline');
-TimelineRoot.setAttribute("layout", "1-column"); 
-let entries = []; 
 const Timeline = () => {
+    const [entries, setEntries] = useState([]);
     useEffect(() => {
-        console.log("Is anything happening anymore?"); 
+        console.log("Why am I not getting here?");
         firebase.auth().onAuthStateChanged((user) => {
             let id = 0;
             if (user) {
@@ -24,44 +22,44 @@ const Timeline = () => {
                 let storageRef = firebase.database().ref('patients/' + id + "/allergies/");
                 storageRef.once('value', function (snapshot) {
                     snapshot.forEach(function (childSnapshot) {
-                        entries.push(childSnapshot);
+                        setEntries([...entries, [childSnapshot, 'allergy']]); 
                         console.log(childSnapshot.child('date').val()); 
                     });
                 });
                 storageRef = firebase.database().ref('patients/' + id + "/appointments/");
                 storageRef.once('value', function (snapshot) {
                     snapshot.forEach(function (childSnapshot) {
-                        entries.push(childSnapshot);
+                        setEntries([...entries, [childSnapshot, 'appointment']]); 
                     });
                 });
                 storageRef = firebase.database().ref('patients/' + id + "/diagnosis/");
                 storageRef.once('value', function (snapshot) {
                     snapshot.forEach(function (childSnapshot) {
-                        entries.push(childSnapshot);
+                        setEntries([...entries, [childSnapshot, 'diagnosis']]); 
                     });
                 });
                 storageRef = firebase.database().ref('patients/' + id + "/medications/");
                 storageRef.once('value', function (snapshot) {
                     snapshot.forEach(function (childSnapshot) {
-                        entries.push(childSnapshot);
+                        setEntries([...entries, [childSnapshot, 'medication']]); 
                     });
                 });
                 storageRef = firebase.database().ref('patients/' + id + "/procedures/");
                 storageRef.once('value', function (snapshot) {
                     snapshot.forEach(function (childSnapshot) {
-                        entries.push(childSnapshot);
+                        setEntries([...entries, [childSnapshot, 'procedure']]); 
                     });
                 });
                 storageRef = firebase.database().ref('patients/' + id + "/tests/");
                 storageRef.once('value', function (snapshot) {
                     snapshot.forEach(function (childSnapshot) {
-                        entries.push(childSnapshot);
+                        setEntries([...entries, [childSnapshot, 'test']]); 
                     });
                 });
-                entries.sort((a, b) => b.date - a.date); 
+                entries.sort((a, b) => b[0].child('date').val() - a[0].child('date').val());
                 console.log(entries); 
             }
-        }); 
+        });
     }, []);
 
     return <VerticalTimeline layout="1-column">{
@@ -69,101 +67,44 @@ const Timeline = () => {
             <VerticalTimelineElement
                 contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                date="2011 - present"
+                date = {entry[0].child('date').val()} 
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                icon={decideIcon(entry[1])}
             >
                 <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>>
-                <h3> Hello World </h3> 
+                <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                <h3> Hello World </h3>
             </VerticalTimelineElement>
         ))
-    }</VerticalTimeline>; 
+    }</VerticalTimeline>;
+}
 
-    /*return (<VerticalTimeline>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-            date="2011 - present"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        >
-            <h3 className="vertical-timeline-element-title">Creative Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-            <p>
-                Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-    </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        >
-            <h3 className="vertical-timeline-element-title">Art Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-                Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-    </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2008 - 2010"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-            <p>
-                User Experience, Visual Design
-    </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2006 - 2008"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-                User Experience, Visual Design
-    </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="April 2013"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        >
-            <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-            <p>
-                Strategy, Social Media
-    </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="November 2012"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        >
-            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-            <p>
-                Creative Direction, User Experience, Visual Design
-    </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="2002 - 2006"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        >
-            <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-            <p>
-                Creative Direction, Visual Design
-    </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-        />
-    </VerticalTimeline>); */
-} 
+function decideIcon(type) {
+    if(type==='allergy')
+        return <FontAwesomeIcon
+        icon={faAllergies}
+      />; 
+      if(type==='diagnosis')
+      return <FontAwesomeIcon
+      icon={faCommentMedical}
+    />; 
+    if(type==='medication')
+        return <FontAwesomeIcon
+        icon={faPills}
+      />; 
+      if(type==='procedure')
+        return <FontAwesomeIcon
+        icon={faProcedures}
+      />; 
+      if(type==='appointment')
+        return <FontAwesomeIcon
+        icon={faCalendar}
+      />; 
+      if(type==='test')
+        return <FontAwesomeIcon
+        icon={faVial}
+      />; 
+}
 
 export default Timeline;
 
